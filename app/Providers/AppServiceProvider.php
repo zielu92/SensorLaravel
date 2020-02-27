83aa1fd7aed4ea53a679644182e2981253d06db0
zielu92 <zielu0@gmail.com>
2020-02-27 21:53:23 +0700

    FIX SQL error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use mysql_xdevapi\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
